a03ae7ba119c42029914ce43cc80002bdeddf900
APM-SYSTEM <APM-SYSTEM@DESKTOP-488FP15>
2020-01-07 15:59:13 +0530

    rtfghf

--- src/test/java/qsp/Demo.java
+++ src/test/java/qsp/Demo.java
@@ -1,5 +1,16 @@
 package qsp;
 
-public class Demo {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo
+{
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.close();
+	}
 
 }
